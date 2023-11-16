25bf1fe95c3c8195b6473f1c56e8088548ec0327
prasetyoary48 <prasetyoary530@student.untan.ac.id>
2023-11-16 18:13:38 +0700

    kit

--- app/controller/api/v2/media.js
+++ app/controller/api/v2/media.js
@@ -1,9 +1,12 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const qr = require('node-qr-image');
+const imagekit = require('../../../../utils/imagekit');
 
 module.exports ={
     uploadImage: async (req, res) => {
         const imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+
         return res.status(200).json({
             status: true,
             message: 'success',
@@ -11,6 +14,60 @@ module.exports ={
                 image_url: imageUrl
             }
         })
+    },
+
+    uploadDocument: async (req, res) => {
+        const documentUrl = `${req.protocol}://${req.get('host')}/files/${req.file.filename}`
+
+        return res.status(200).json({
+            status: true,
+            message: 'success',
+            data: {
+                image_url: documentUrl
+            }
+        })
+    },
+
+    uploadVideo: async (req, res) => {
+        const videoUrl = `${req.protocol}://${req.get('host')}/files/${req.file.filename}`
+
+        return res.status(200).json({
+            status: true,
+            message: 'success',
+            data: {
+                video_url: videoUrl
+            }
+        })
+    },
+
+    qrcode: async (req, res) => {
+        const { text } = req.body;
+
+        const qrCode = qr.image(text, { type: 'png' });
+        res.setHeader("Content-Type", "image/png")
+        qrCode.pipe(res);
+    },
+
+    imagekitUpload: async(req, res) => {
+        try {
+            const stringFile = req.file.buffer.toString('base64'); 
+            
+            const uploadFile = await imagekit.upload({
+                fileName: req.file.originalname,
+                file: stringFile
+            })
+            
+            return res.status(200).json({
+                status: 'OK',
+                message: 'Success',
+                data: {
+                    name: uploadFile.name,
+                    url: uploadFile.url,
+                    type: uploadFile.fileType
+                }
+            })
+        } catch(err) {
+            throw err;
+        }
     }
-    
 }
--- router/media.js
+++ router/media.js
@@ -2,9 +2,20 @@ const express = require('express');
 const router = express.Router();
 const storage = require('../utils/multer')
 const controller = require('../app/controller')
+const multer = require('multer')();
 
 router.get('/images', express.static('public/images'))
 
 router.post('/api/v2/upload/image', storage.image.single('image'),controller.media.uploadImage)
+router.post('/api/v2/upload/video', storage.video.single('video'),controller.media.uploadVideo)
+router.post('/api/v2/upload/documents', storage.document.single('doc'),controller.media.uploadDocument)
+
+//qrcode
+router.post('/api/v2/qrcode', controller.media.qrcode)
+
+//imagekit
+router.post('/api/v2/upload/imagekit',multer.single('image'),controller.media.imagekitUpload
+)
+
 
 module.exports = router;
